5c000ce579fe827162485f3ad1b831ce3a2469e4
alpha87 <jianxun2004@gmail.com>
2020-03-03 21:35:30 +0800

    更新

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="pysmms",
-    version="0.0.7",
+    version="0.0.8",
     author="Jianxun",
     author_email="i@lijianxun.top",
     description="The CLI Tool for SM.MS, based on API v2.",
